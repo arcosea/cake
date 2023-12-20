551445954237510efc1aed17387c47849830c90d
Erick Arcos <arcosea@plu.edu>
2023-12-20 14:16:05 -0800

    Updated: itemcounter now handles input textfield changes

--- src/ui/components/ItemCounter.tsx
+++ src/ui/components/ItemCounter.tsx
@@ -4,9 +4,11 @@ import { GrSubtractCircle } from "react-icons/gr";
 import { IoIosAddCircleOutline } from "react-icons/io"
 
 export default function ItemCounter(){
-    const [count, setCount] = useState(0);
+    const minCount: number = 0;
+    const maxCount: number = 100;
 
-    const updateCount = (value: number) => {
+    const [count, setCount] = useState(0);
+    const handleButtonCountChange = (value: number) => {
         if(value <= 0){
             setCount(0)
         } else {
@@ -14,29 +16,36 @@ export default function ItemCounter(){
         }
     }
 
+    const handleInputCountChange = (event: any) => {
+        let value = Number(event.target.value);
+        if(!value || value <= minCount || value > maxCount){
+            setCount(0)
+        } else {
+            setCount(value)
+        }
+    }
 
     return (
         <>
             <Stack direction="row" spacing={2}>
                 <Button
                     variant="outlined" color="error" 
-                    onClick={() => updateCount(count - 1)}
+                    onClick={() => handleButtonCountChange(count - 1)}
                     size="small"
                     >
                     <GrSubtractCircle style={{ color: 'red', fontSize: "16px"}} />
                 </Button>
                 <TextField
-                    id="outlined-number"
                     label="Quantity"
-                    type="number"
                     value={count}
                     InputLabelProps={{
                         shrink: true,
                     }}
+                    onChange={handleInputCountChange}
                 />
                 <Button
                     variant="outlined" color="success" 
-                    onClick={() => updateCount(count + 1)}
+                    onClick={() => handleButtonCountChange(count + 1)}
                     size="small"
                     >
                     <IoIosAddCircleOutline style={{ color: 'green', fontSize: "16px"}} />
--- src/ui/device/Mobile.tsx
+++ src/ui/device/Mobile.tsx
@@ -22,7 +22,7 @@ export function Mobile(){
                 />
             ))}
             
-            <CheckoutDrawer />
+            {/* <CheckoutDrawer /> */}
             
            
         </>
